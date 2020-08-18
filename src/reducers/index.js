5c7178961a2c072a4a9d83b874d4ed99bcd455da
Dylan Morison <dmorison@oxy.edu>
2020-08-17 17:34:49 -0700

    signup feature working properly

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combinbeReducers, combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
 import auth from './auth';
 
 export default combineReducers({
-    auth
+    auth,
+    form: formReducer
 });
